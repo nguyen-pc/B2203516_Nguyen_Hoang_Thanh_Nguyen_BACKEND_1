dc2545c9e0b664ad5362bf855c32c46b1953bf38
nguyen <nguyenthcs430@gmail.com>
2024-09-19 15:19:28 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+const app = express();
+
+app.use(cors());
+
+app.use((err, req, res, next) => {
+  return res
+    .status(err.statusCode || 500)
+    .json({ message: err.message || "Internal Server Error" });
+});
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to contact book application" });
+});
+
+//handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+module.exports = app;
---
+++ app/config/index.js
@@ -0,0 +1,9 @@
+const app = require("../../app");
+
+const config = {
+  app: {
+    port: process.env.PORT || 3000,
+  },
+};
+
+module.exports = config;
